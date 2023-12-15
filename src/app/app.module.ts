97f9f3e6dd67c8f2b883417739e8dcbd3262b9d1
claudia.quezada <claudia.quezada@tisal.cl>
2023-12-15 11:35:52 -0300

    initialize modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,17 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { FormTemplateModule } from './form-template/form-template.module';
+import { FormReactiveModule } from './form-reactive/form-reactive.module';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormTemplateModule,
+    FormReactiveModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
