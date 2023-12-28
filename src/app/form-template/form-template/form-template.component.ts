83997905dba342a05bd3394943816b4c0b46abd4
claudia.quezada <claudia.quezada@tisal.cl>
2023-12-28 14:53:06 -0300

    form viewChild

--- src/app/form-template/form-template/form-template.component.ts
+++ src/app/form-template/form-template/form-template.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-form-template',
@@ -7,8 +8,11 @@ import { Component } from '@angular/core';
 })
 export class FormTemplateComponent {
 
-  agregar(form: any) {
-    console.log('Enviado:',form);
+  @ViewChild("templateForm")
+  templateForm!: NgForm;
+
+  agregar() {
+    console.log('templateForm:', this.templateForm.value);
     
   }
 }
