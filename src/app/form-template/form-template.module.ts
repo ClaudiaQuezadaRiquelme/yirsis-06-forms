2eaeb1334706e24af7f0c689a34e6023f0c4f774
claudia.quezada <claudia.quezada@tisal.cl>
2023-12-28 14:11:32 -0300

    angular FormsModule

--- src/app/form-template/form-template.module.ts
+++ src/app/form-template/form-template.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormTemplateComponent } from './form-template/form-template.component';
+import { FormsModule } from '@angular/forms';
 
 
 
@@ -9,7 +10,8 @@ import { FormTemplateComponent } from './form-template/form-template.component';
     FormTemplateComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    FormsModule,
   ],
   exports: [
     FormTemplateComponent
